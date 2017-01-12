320a5f54935ca3ab00ef39efb9da32e6813da094
Vien Tang <vientang@gmail.com>
2017-01-11 19:40:39 -0800

    Fix merge conflicts

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-
 var express  = require('express'),
     Twitter  = require('twitter'),
+    Promise  = require('bluebird'),
     watson   = require('watson-developer-cloud'),
     bodyParser = require('body-parser'), // middleware to get data from forms. Express can't do this.
     ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3'),
@@ -15,6 +15,7 @@ app.set('view engine', 'ejs');
 // middleware to read JSON data
 app.use(bodyParser.urlencoded({extended: true}) )
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
 
 //========GET REQUEST FOR HOMEPAGE=======
 app.get('/', function(req, res) {
@@ -36,9 +37,44 @@ var tone_analyzer = new ToneAnalyzerV3({
   version_date: '2016-05-19'
 });
 
+//========Helper functions=======
+var getHighestToneScore = function(tones) {
+  var emotionTones = tones.document_tone.tone_categories[0].tones;  
+  return emotionTones.reduce(function(tone1, tone2) {
+    return tone1.score > tone2.score ? tone1 : tone2;
+  });
+}
+
+var getTextTweets = function(arrayOfTweets) {
+  return arrayOfTweets.statuses.map(function(tweetData) {
+    return tweetData.text;
+  });
+}
+
+var arrayOfTweets = function(messages) {
+  return messages.map(function(messagesArray) {
+    return messagesArray.text;
+  });
+}
+
+var getToneData = function(scoreData) {
+  console.log("Score Data from getToneData:", scoreData);
+  return scoreData;
+}
+
 //========Call API's=======
+var getTweetData = function(keyword) {
+  return new Promise(function(resolve, reject) {
+    client.get(`https://api.twitter.com/1.1/search/tweets.json?q=${keyword}&count=10`, function(error, tweets, response) {
+      if(error) console.log(error);      
+      resolve(tweets);   
+    });
+  });
+};
+
 app.post('/searchKeyword', function(req, res){
   var keyword = req.body.keyword;
+
 
 
   client.get(`https://api.twitter.com/1.1/search/tweets.json?q=${keyword}&count=100`, function(error, tweets, response) {
@@ -79,6 +115,19 @@ app.post('/searchKeyword', function(req, res){
 
   }); // end Twitter Call
 });
+
+
+//   getTweetData(keyword)
+//   .then(function(data) {
+//     var scoreData = arrayOfTweets(data.statuses); 
+//     // use scoreData to pass into toneAnalyzer
+//     // use toneAnalyzer to get tones
+//     // res.send data from toneAnalyzer
+//     res.send(scoreData);
+//   }).catch(function(err) {
+//     console.log(err);
+//   });
+// });
 
 
 app.listen('5000', function(){
